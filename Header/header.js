a2767e2b7633b4904dc9e778f16abf37f89900ea
Shivam Kumar <shivam2432000@gmail.com>
2022-06-17 23:45:47 +0530

    This is Final Update

--- Header/header.js
+++ Header/header.js
@@ -1,4 +1,24 @@
-document.querySelector("navbarThird:last-child()").addEventListener("click",show);
-function show(){
+document.querySelector("#cart").addEventListener("click",show);
+function show(event){
+    event.preventDefault();
     document.querySelector("#navbarSection1").style.visibility="visible";
 }
+document.querySelector("#cross").addEventListener("click",show1);
+function show1(event){
+    event.preventDefault();
+    document.querySelector("#navbarSection1").style.visibility="hidden";
+}
+
+document.querySelector("#menu").addEventListener("click",show2);
+function show2(event){
+    event.preventDefault();
+    document.querySelector("#menuSection").style.visibility="visible";
+}
+document.querySelector("#menu").addEventListener("dblclick",show3);
+function show3(event){
+    event.preventDefault();
+    document.querySelector("#menuSection").style.visibility="hidden";
+}
+
+
+
